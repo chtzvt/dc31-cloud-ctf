fb8012c2a1273d14885f2692eaffc99391f5c8ee
Charlton Trezevant <ct@ctis.me>
2023-08-12 20:45:25 -0700

    Incremental commit

--- create_sa_pk.py
+++ create_sa_pk.py
@@ -104,20 +104,100 @@ def create_service_account_key(sa_name):
         print("Unable to create credentials for " + sa_name)
         return "{}"
 
+def impersonate_service_account(original_token, target_service_account_email, delegates=[]):
+    GOOGLE_IMPERSONATE_URL = f"https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/{target_service_account_email}:generateAccessToken"
+    
+    headers = {
+        "Authorization": f"Bearer {original_token}"
+    }
+    
+    data = {
+        "lifetime": "3600s",  # Duration for which the impersonated token is valid
+        "scope": ["https://www.googleapis.com/auth/cloud-platform"],  # The OAuth2 scopes for the generated token
+        "delegates": delegates
+    }
+
+    print(data)
+    
+    response = requests.post(GOOGLE_IMPERSONATE_URL, headers=headers, json=data)
+
+    if response.status_code != 200:
+        print("Error impersonating service account:", response.json())
+        return None
+
+    return response.json()['accessToken']
+
+
+def list_service_account_permissions(service_accounts, access_token):
+    base_url = "https://iam.googleapis.com/v1"
+    headers = {
+        "Authorization": f"Bearer {access_token}",
+        "Accept": "application/json"
+    }
+    
+    permissions_by_account = {}
+    
+    for sa_email in service_accounts:
+        resource = f"projects/-/serviceAccounts/{sa_email}"
+        policy_url = f"{base_url}/{resource}:getIamPolicy"
+        response = requests.post(policy_url, headers=headers)
+        
+        if response.status_code != 200:
+            print(f"Failed to get IAM policy for {sa_email}. HTTP Status: {response.status_code}")
+            continue
+
+        policy = response.json()
+        roles = {binding['role'] for binding in policy.get('bindings', [])}
+        
+        permissions = set()
+        for role in roles:
+            role_info_url = f"{base_url}/{role}"
+            role_response = requests.get(role_info_url, headers=headers)
+            
+            if role_response.status_code != 200:
+                print(f"Failed to get role information for {role}. HTTP Status: {role_response.status_code}")
+                continue
+                
+            role_info = role_response.json()
+            role_permissions = role_info.get('includedPermissions', [])
+            permissions.update(role_permissions)
+        
+        permissions_by_account[sa_email] = permissions
+
+    return permissions_by_account
+
 def write_credential(sa_name, sa_content):
     with open("./accounts/" + sa_name + ".json", 'wb') as file:
         file.write(bytes(str(sa_content), "UTF-8"))
         file.close()
 
+def format_delegates(service_accounts):
+    return [f"projects/-/serviceAccounts/{sa}" for sa in service_accounts]
+
 
 # Variables
 project_id = 'optimal-jigsaw-390814'
 access_token = get_access_token()
-print(list_service_accounts(project_id, access_token))
 
-service_accounts = ['leakybucket@optimal-jigsaw-390814.iam.gserviceaccount.com', 'leakysecret@optimal-jigsaw-390814.iam.gserviceaccount.com', '134663715059-compute@developer.gserviceaccount.com', 'leakyaccess@optimal-jigsaw-390814.iam.gserviceaccount.com', 'docker-pusher@optimal-jigsaw-390814.iam.gserviceaccount.com', 'optimal-jigsaw-390814@appspot.gserviceaccount.com']
-service_account_name = 'leakysecret@optimal-jigsaw-390814.iam.gserviceaccount.com'
+service_accounts = [
+    #'leakybucket@optimal-jigsaw-390814.iam.gserviceaccount.com',
+    'leakysecret@optimal-jigsaw-390814.iam.gserviceaccount.com',
+    '134663715059-compute@developer.gserviceaccount.com',
+    #'leakyaccess@optimal-jigsaw-390814.iam.gserviceaccount.com',
+    #'docker-pusher@optimal-jigsaw-390814.iam.gserviceaccount.com',
+    #'optimal-jigsaw-390814@appspot.gserviceaccount.com'
+]
 
-for sa_name in service_accounts:
-    print("Creating key for " + sa_name)
-    write_credential(sa_name, create_service_account_key(sa_name))
+#print(list_service_account_permissions(service_accounts, access_token))
+
+impersonated_token = impersonate_service_account(get_access_token(), 'leakysecret@optimal-jigsaw-390814.iam.gserviceaccount.com', delegates=format_delegates(service_accounts))
+
+print(impersonated_token)
+
+#print(list_service_accounts(project_id, access_token))
+
+#service_account_name = 'leakysecret@optimal-jigsaw-390814.iam.gserviceaccount.com'
+
+#for sa_name in service_accounts:
+#    print("Creating key for " + sa_name)
+#    write_credential(sa_name, create_service_account_key(sa_name))
